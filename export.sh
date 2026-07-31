7b73531fcc32533a539fec8dc12af082775f1770
LukeV37 <lvaughan@anl.gov>
2026-07-31 16:02:59 -0500

    update paths

--- export.sh
+++ export.sh
@@ -1,11 +1,6 @@
 # Repo-local defaults for Time_Series_Anomaly_Detection.
 
-# Source this to override the Time_Series_Anomaly_Detection venv location.
 export PBEAST_VENV_DIR="/afs/cern.ch/user/l/lvaughan/VirtualEnvs/venv_pbeast"
-
-# Optional overrides:
-# export PBEAST_VENV_DIR="/afs/cern.ch/user/l/lvaughan/VirtualEnvs/venv_time_series_anomaly_detection"
-# export TDAQ_RELEASE="tdaq-12-00-00"
 export ATLAS_DATA_DIR="/lcrc/project/AIDQ/Atlas/pbeast_fetcher/fetch_by_runs_v3/"
 export SPT_DATA_DIR="/lcrc/project/SPT3G/users/ac.weiquan/ml_dqm/calibrator_responses/operable_detectors_all_obs"
 export OUTPUT_DIR="/lcrc/project/AIDQ/users/lvaughan/run/"
