65f5ab5400e9850476e10571c0aed5b5f21b619b
LukeV37 <lvaughan@anl.gov>
2026-07-31 15:40:33 -0500

    add data paths

--- export.sh
+++ export.sh
@@ -6,3 +6,7 @@ export PBEAST_VENV_DIR="/afs/cern.ch/user/l/lvaughan/VirtualEnvs/venv_pbeast"
 # Optional overrides:
 # export PBEAST_VENV_DIR="/afs/cern.ch/user/l/lvaughan/VirtualEnvs/venv_time_series_anomaly_detection"
 # export TDAQ_RELEASE="tdaq-12-00-00"
+export ATLAS_DATA_DIR="/lcrc/project/AIDQ/Atlas/pbeast_fetcher/fetch_by_runs_v3/"
+export SPT_DATA_DIR="/lcrc/project/SPT3G/users/ac.weiquan/ml_dqm/calibrator_responses/operable_detectors_all_obs"
+export OUTPUT_DIR="/lcrc/project/AIDQ/users/lvaughan/run/"
+export VENV_DIR="/lcrc/project/AIDQ/users/lvaughan/envs"
