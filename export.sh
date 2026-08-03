36183cf88ca07a1e7d6324e6cf75808dc57577dd
Luke Martin Vaughan <luke.martin.vaughan@cern.ch>
2026-08-03 18:35:37 +0200

    move pbeast data dir

--- export.sh
+++ export.sh
@@ -9,5 +9,5 @@ export VENV_DIR="/lcrc/project/AIDQ/users/lvaughan/envs"
 
 # Data directories
 export ATLAS_DATA_DIR="/lcrc/project/AIDQ/Atlas/pbeast_fetcher/fetch_by_runs_v3/"
-export PBEAST_HTML_DIR="/lcrc/project/AIDQ/users/lvaughan/data/DataSummary"
+export PBEAST_HTML_DIR="/afs/cern.ch/user/l/lvaughan/public/ATLASDataSummary"
 export SPT_DATA_DIR="/lcrc/project/SPT3G/users/ac.weiquan/ml_dqm/calibrator_responses/operable_detectors_all_obs"
