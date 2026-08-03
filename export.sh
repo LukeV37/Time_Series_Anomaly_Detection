dadee67347f4568565c6ae93dbc7758d23f0c7a2
LukeV37 <lvaughan@anl.gov>
2026-08-03 09:25:08 -0500

    edit paths

--- export.sh
+++ export.sh
@@ -1,8 +1,13 @@
 # Repo-local defaults for Time_Series_Anomaly_Detection.
 
+# Bulk Storage
+export OUTPUT_DIR="/lcrc/project/AIDQ/users/lvaughan/run/"
+
+# Virtual Environments
 export PBEAST_VENV_DIR="/afs/cern.ch/user/l/lvaughan/VirtualEnvs/venv_pbeast"
+export VENV_DIR="/lcrc/project/AIDQ/users/lvaughan/envs"
+
+# Data directories
 export ATLAS_DATA_DIR="/lcrc/project/AIDQ/Atlas/pbeast_fetcher/fetch_by_runs_v3/"
 export PBEAST_HTML_DIR="/lcrc/project/AIDQ/users/lvaughan/data/DataSummary"
 export SPT_DATA_DIR="/lcrc/project/SPT3G/users/ac.weiquan/ml_dqm/calibrator_responses/operable_detectors_all_obs"
-export OUTPUT_DIR="/lcrc/project/AIDQ/users/lvaughan/run/"
-export VENV_DIR="/lcrc/project/AIDQ/users/lvaughan/envs"
