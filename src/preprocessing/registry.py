9bfbca137f3ca4f852099d2977366102e8b3b952
LukeV37 <lvaughan@anl.gov>
2026-08-11 14:40:38 -0500

    register trim_edges and remaining shape-preserving steps

--- src/preprocessing/registry.py
+++ src/preprocessing/registry.py
@@ -4,18 +4,24 @@ from __future__ import annotations
 
 from collections.abc import Callable
 
-from .transforms.filters import drop_nan_channels, drop_nan_timesteps
-from .transforms.imputer import fill_channel_median
+from .transforms.filters import drop_nan_channels, drop_nan_timesteps, trim_edges
+from .transforms.imputer import fill_channel_mean, fill_channel_median
 from .transforms.normalizer import clip_values
-from .transforms.transforms import fill_nan
+from .transforms.reducer import subsample_time
+from .transforms.transforms import drop_features, fill_nan, keep_features
 
 
 STEP_MAP: dict[str, Callable[..., object]] = {
     "drop_nan_channels": drop_nan_channels,
     "drop_nan_timesteps": drop_nan_timesteps,
+    "trim_edges": trim_edges,
     "fill_channel_median": fill_channel_median,
+    "fill_channel_mean": fill_channel_mean,
     "clip_values": clip_values,
     "fill_nan": fill_nan,
+    "subsample_time": subsample_time,
+    "drop_features": drop_features,
+    "keep_features": keep_features,
 }
 
 
